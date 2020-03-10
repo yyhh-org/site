bb8c44b38e257003de30c7b7e9fd62554012d4f5
Huahai Yang <huahai.yang@gmail.com>
2020-03-10 05:35:13 -0700

    publish blog

--- tasks.py
+++ tasks.py
@@ -112,7 +112,7 @@ def publish(c):
     """Publish to production via rsync"""
     c.run('pelican -s {settings_publish}'.format(**CONFIG))
     c.run(
-        'rsync --delete --exclude ".DS_Store" -pthrvz -c '
+        'rsync --delete --exclude ".DS_Store" --exclude ".!*-e" -pthrvz -c '
         '-e "ssh -p {ssh_port}" '
         '{} {ssh_user}@{ssh_host}:{ssh_path}'.format(
             CONFIG['deploy_path'].rstrip('/') + '/',
