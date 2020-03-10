c299016bfccbe223f8ca87bede86dfac968c28c9
Huahai Yang <huahai.yang@gmail.com>
2020-03-10 15:17:29 -0700

    add rss icon

--- publishconf.py
+++ publishconf.py
@@ -16,9 +16,6 @@ RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 # CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-MENUITEMS = [
-    ['RSS', "feeds/all.atom.xml"]
-]
 
 DELETE_OUTPUT_DIRECTORY = True
 
